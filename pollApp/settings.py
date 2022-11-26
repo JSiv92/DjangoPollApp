b0d282929fe19b4948ebf6509b7d7d51ca9fe26f
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-11-26 23:18:53 +1300

    migrated  models and made  string methods

--- pollApp/settings.py
+++ pollApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', #path to the 'polls' app (see polls/apps.py)
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
