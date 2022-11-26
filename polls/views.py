1d08abccd9d4a238899a26b3d5aa3fbefccf7bb8
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-11-26 20:18:42 +1300

    after first migration - created identity tables etc

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+#http://localhost:8000/polls/
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Polls>Index")
+    return HttpResponse("hi pup")
 
