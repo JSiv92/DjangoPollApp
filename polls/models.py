6deb68b4c268b3f084d28c597ac08e0358a2c22e
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-11-30 23:33:55 +1300

    added more empty views to polls

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
     
     def __str__(self):
@@ -21,4 +21,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     
     def __str__(self):
-        return self.choice_text
+        return self.choice_text 
+    
