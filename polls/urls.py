437189e843892c07c5df6487b35917eab67259b2
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-12-03 18:18:51 +1300

    changed index,detail and results to django generic views

--- polls/urls.py
+++ polls/urls.py
@@ -7,8 +7,8 @@ from . import views
 # app_name so Django knows which app view to create for a url when using the {% url %} template tag
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('specifics<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
