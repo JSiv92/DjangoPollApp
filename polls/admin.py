b11110c47c8e1b17c07b322ee962143ac479ebae
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-12-06 17:26:38 +1300

    added Choice class to polls admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Question
 
-# Register your models here.
-admin.site.register(Question) 
+from .models import Question, Choice
+
+#split admin form for Question into categories with fieldsets 
+# (give them their own headings like Date information)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
