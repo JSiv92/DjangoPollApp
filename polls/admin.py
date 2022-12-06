2ad305be7a5dc7dfe43ef677bc626ce555478f94
Jonathan Sivananthan <jonathan1512@gmail.com>
2022-12-06 17:38:57 +1300

    made second admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
 
-#split admin form for Question into categories with fieldsets 
-# (give them their own headings like Date information)
+# allow choices to be set when adding question inb admin ui
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin) # register to admin ui
